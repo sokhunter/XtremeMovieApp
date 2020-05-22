56888e05192ef8dd09cd93624827425b2c1c0237
Stefany Livaque <emylivaque@gmail.com>
2020-05-22 02:15:01 -0500

    Agregar y listar en favoritos

--- app/src/main/java/com/example/xtrememovieapp/Favorite.kt
+++ app/src/main/java/com/example/xtrememovieapp/Favorite.kt
@@ -2,9 +2,14 @@ package com.example.xtrememovieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_favorite.*
 
-class Favorite : AppCompatActivity() {
+class Favorite : AppCompatActivity(), OnItemClickListener {
+
+    override fun OnItemClicked(movie: Movie) {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +18,20 @@ class Favorite : AppCompatActivity() {
         btnReturn.setOnClickListener {
             finish()
         }
-        
+        loadFavorite()
     }
+
+    lateinit var movies: List<Movie>
+    lateinit var movieAdapter: MovieAdapter
+
+    private fun loadFavorite() {
+        movies = AppDatabase.getInstance(this).getDao().getAll()
+        movieAdapter = MovieAdapter(movies, this)
+        rvFavorite.adapter = movieAdapter
+        rvFavorite.layoutManager = LinearLayoutManager(this)
+    }
+
+
+
+
 }
